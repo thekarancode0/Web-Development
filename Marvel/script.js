d42a2bb727c89e21da5bd66439bcdd4cce18be47
Karan Kumar <mrkarankr87@gmail.com>
2024-11-04 11:21:59 +0530

    Add files via upload
    A simple landing page for marvel fans

---
+++ Marvel/script.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click',()=>{
+    sidebar.classList.toggle('view-this');
+});
+
